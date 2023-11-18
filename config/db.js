cfa5d630d1dd27726638ae61e39f2cce117c04de
Adhi <adhinarayanan619@gmail.com>
2023-11-18 18:42:16 +0530

    deploy

--- config/db.js
+++ config/db.js
@@ -4,11 +4,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://adhi1:Shishi2648@cluster0.ipjzefl.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
